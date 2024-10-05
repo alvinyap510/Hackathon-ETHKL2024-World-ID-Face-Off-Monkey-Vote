b255f630613460e3935bebdb2bd875bcb8895602
Alvin Yap <leaf.alvin@gmail.com>
2024-10-05 08:10:02 +0800

    feat: add layer zero router cross chain messaging

---
+++ smart-contracts/src/LayerZeroWorldIdRelayer.sol
@@ -0,0 +1,8 @@
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.24;
+
+// import {OApp, Origin, MessagingFee} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
+
+// contract LayerZeroWorldIdRelayer is OApp {
+//     constructor(address _endpoint) OApp(_endpoint, msg.sender) {}
+// }
