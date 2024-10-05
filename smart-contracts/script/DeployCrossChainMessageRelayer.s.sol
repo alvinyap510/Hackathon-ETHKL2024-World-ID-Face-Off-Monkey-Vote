beca41af91230a47937fa46f73d805600f8ec7a5
Alvin Yap <leaf.alvin@gmail.com>
2024-10-05 17:54:30 +0800

    feat: add deploy op sepolia and scroll sepolia contract

--- smart-contracts/script/DeployCrossChainMessageRelayer.s.sol
+++ smart-contracts/script/DeployCrossChainMessageRelayer.s.sol
@@ -20,5 +20,16 @@ contract DeployCrossChainMessageRelayer is Script {
 
 // Scroll Sepolia LZ Endpoint: 0x6EDCE65403992e310A62460808c4b910D972f10f
 // Manta Sepolia LZ Endpoint: 0x6EDCE65403992e310A62460808c4b910D972f10f
+// Optimism Sepolia LZ Endpoint: 0x6EDCE65403992e310A62460808c4b910D972f10f
+
+// ScrollSepoliaRelayer: 0x79E40811762DD0702fd280E125AF2fA32d7F54Be
+// OptimismSepolia: 0x9e45B76F4dCb0A4d1b7eA7bab031f6d880DB5Cd1
+// MantaSepoliaRelayer:
+
+// Scroll
+// forge script script/DeployCrossChainMessageRelayer.s.sol:DeployCrossChainMessageRelayer --rpc-url https://sepolia-rpc.scroll.io --broadcast --verify --etherscan-api-key 8654MU4RD3WIRD3QM342QG35BYDP9CVAEG --verifier-url https://api-sepolia.scrollscan.com/api -vvvv
+
+// Optimism
+// forge script script/DeployCrossChainMessageRelayer.s.sol:DeployCrossChainMessageRelayer --rpc-url https://sepolia.optimism.io --broadcast --verify --etherscan-api-key HRS7T3ISZ9Z4YD4135QQ6IDKU1G6GWAEFZ --verifier-url https://api-sepolia-optimistic.etherscan.io/api -vvvv
 
 // forge script script/DeployCrossChainMessageRelayer.s.sol:DeployCrossChainMessageRelayer --rpc-url https://sepolia-rpc.scroll.io --broadcast --verify --etherscan-api-key 8654MU4RD3WIRD3QM342QG35BYDP9CVAEG --verifier-url https://api-sepolia-optimistic.etherscan.io/api -vvvv
